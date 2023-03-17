3b81005a618750e08d90350bebe9c1523ed33771
DlopezCR <117857133+DlopezCR@users.noreply.github.com>
2023-03-17 10:14:55 -0600

    Add files via upload

---
+++ MercedesBenzApiAfis/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using MercedesBenzLogger;
+using Serilog;
+
+namespace MercedesBenzApiAfis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = CustomLogger.CustomLoggerConfiguration();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .UseSerilog()
+            ;
+    }
+}
---
+++ MercedesBenzApiAfis/Startup.cs
@@ -0,0 +1,58 @@
+using MercedesBenzApiAfis.Contracts;
+using MercedesBenzDBContext;
+using MercedesBenzLibrary;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace MercedesBenzApiAfis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.AddHeaderForwarded();
+
+            services.AddSingleton<ApplicationDBContext>();
+            services.AddScoped<IAfiRepository, AfiRepository>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MercedesBenzApiAfis", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MercedesBenzApiAfis v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
